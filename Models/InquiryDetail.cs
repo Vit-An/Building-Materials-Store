313a834167a7d9cb0b57f2699e48d5b842ca6eeb
Vitaliy Bigun <vitaliy.bigun@swanteams.com>
2021-08-03 14:47:10 +0300

    Create models InquiryHeader And InquiryDetail

---
+++ Models/InquiryDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+    public class InquiryDetail
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int InquaryHeaderId { get; set; }
+        [ForeignKey("InquaryHeaderId")]
+        public InquiryHeader InquaryHeader { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+    }
+}
---
+++ Models/InquiryHeader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Models
+{
+    public class InquiryHeader
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public DateTime InquiryDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
