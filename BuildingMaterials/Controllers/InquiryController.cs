53c3bd0872f7e1b092104e359a5e2ac5d85e6d5d
Vitaliy Bigun <vitaliy.bigun@swanteams.com>
2021-08-04 12:30:39 +0300

    Add View for Inquiry

---
+++ BuildingMaterials/Controllers/InquiryController.cs
@@ -0,0 +1,57 @@
+﻿using Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utility;
+using DataAccess;
+using DataAccess.Repository.IRepisitory;
+using DataAccess.Repository;
+using Models.ViewModels;
+
+namespace BuildingMaterials.Controllers
+{
+
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+
+        [BindProperty]
+        public InquiryVM InquiryVM { get; set; }
+
+        public InquiryController(IInquiryHeaderRepository inqHRepo,
+            IInquiryDetailRepository inqDRepo)
+        {
+            _inqHRepo = inqHRepo;
+            _inqDRepo = inqDRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            InquiryVM = new InquiryVM()
+            {
+                InquiryHeader = _inqHRepo.FirstOrDefault(u => u.Id == id),
+                InquiryDetail = _inqDRepo.GetAll(u=>u.InquaryHeaderId == id, includeProperties:"Product")
+            };
+
+            return View(InquiryVM);
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetInquiryList()
+        {
+            return Json(new { data = _inqHRepo.GetAll() });
+        }
+
+        #endregion
+
+    }
+}
