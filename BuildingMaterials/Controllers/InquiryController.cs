c1f561f72937f3a213cd5bda42f95cf15e0e58ac
Vitaliy Bigun <vitaliy.bigun@swanteams.com>
2021-08-04 16:22:06 +0300

    Add method convertToCart in Inquiry

--- BuildingMaterials/Controllers/InquiryController.cs
+++ BuildingMaterials/Controllers/InquiryController.cs
@@ -43,6 +43,27 @@ namespace BuildingMaterials.Controllers
 
             return View(InquiryVM);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            InquiryVM.InquiryDetail = _inqDRepo.GetAll(u => u.InquaryHeaderId == InquiryVM.InquiryHeader.Id);
+
+            foreach(var detail in InquiryVM.InquiryDetail)
+            {
+                ShoppingCart shoppingCart = new ShoppingCart()
+                {
+                    ProductId = detail.ProductId
+                };
+                shoppingCartList.Add(shoppingCart);
+            }
+            HttpContext.Session.Clear();
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            HttpContext.Session.Set(WC.SessionInquiryId, InquiryVM.InquiryHeader.Id);
+
+            return RedirectToAction("Index","Cart");
+        }
 
         #region API CALLS
         [HttpGet]
