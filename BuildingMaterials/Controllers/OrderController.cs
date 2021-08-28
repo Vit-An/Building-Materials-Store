6520b391ea6ee8b7401984efed79a587f0817545
Vitaliy Bigun <vitaliy.bigun@swanteams.com>
2021-08-28 19:23:35 +0300

    Add Order List View Model

---
+++ BuildingMaterials/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Repository.IRepisitory;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utility.BrainTree;
+
+namespace BuildingMaterials.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHRepo;
+        private readonly IOrderDetailRepository _orderDRepo;
+        private readonly IBrainTreeGate _brain;
+
+        public OrderController(
+        IOrderHeaderRepository orderHRepo, IOrderDetailRepository orderDRepo, IBrainTreeGate brain)
+        {
+            _brain = brain;
+            _orderDRepo = orderDRepo;
+            _orderHRepo = orderHRepo;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
